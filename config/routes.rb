35f5107c60cf534de2cef078b4fd2fe77bfebea8
James Barker <jbarkerlouth@gmail.com>
2022-11-28 15:53:55 +0100

    added ships controller with index/new/create db ships

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  root to: "ships#index"
 
-  resources :ships, only: %i[index new create]
+  resources :ships
 end
