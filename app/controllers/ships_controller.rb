106ab13724f6c5c7a94527f32333836030fc4787
James Barker <jbarkerlouth@gmail.com>
2022-11-28 15:09:17 +0100

    added the ships controller and index/new views

---
+++ app/controllers/ships_controller.rb
@@ -0,0 +1,24 @@
+class ShipsController < ApplicationController
+  def index
+    @ships = Ship.all
+  end
+
+  def new
+    @ship = Ship.new
+  end
+
+  def create
+    @ship = Ship.new(ship_params)
+    if @ship.save
+      redirec_to ship_path(@ship)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def ship_params
+    params.require(:ship).permit(:name, :size, :price)
+  end
+end
