03811fe192c1ce0170ea8567473c1c88f0049f18
James Barker <jbarkerlouth@gmail.com>
2022-11-28 16:22:23 +0100

    added bookings db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_141632) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_152035) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "ship_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ship_id"], name: "index_bookings_on_ship_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "ships", force: :cascade do |t|
     t.string "name"
@@ -37,5 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_141632) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "ships"
+  add_foreign_key "bookings", "users"
   add_foreign_key "ships", "users"
 end
