334e2304ab1f7b0a293ccb4ec302125164414a2e
James Barker <jbarkerlouth@gmail.com>
2022-11-29 12:39:54 +0100

    added the bookings controller and associated view pages

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -1,5 +1,4 @@
 class ShipsController < ApplicationController
-
   def index
     @ships = Ship.all
   end
@@ -10,10 +9,9 @@ class ShipsController < ApplicationController
 
   def create
     @ship = Ship.new(ship_params)
-    @ship.user = User.last
-    # current_user - This replaced User.last. Waiting for login Gem to be finished.
+    @ship.user = current_user
     if @ship.save!
-      redirect_to ships_path
+      redirect_to root_path
     else
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "ships#index"
 
-  resources :ships, except: :index
+  resources :ships, except: :index do
+    resources :bookings
+  end
 end
